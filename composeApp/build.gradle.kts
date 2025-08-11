66554e265a4c4d0f75f32afc13208993509ccfd6
pr3y <67231282+pr3y@users.noreply.github.com>
2025-08-11 20:21:59 -0300

    fdroid version fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "bruce.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3"
     }
 
     signingConfigs {
